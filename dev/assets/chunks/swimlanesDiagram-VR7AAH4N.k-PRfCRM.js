d94ce267e21db4325f10243b64a607c41f9aee1b
Documenter.jl <documenter@juliadocs.github.io>
2026-09-10 07:52:56 +0000

    build based on 69fcbc8

--- dev/assets/chunks/swimlanesDiagram-VR7AAH4N.k-PRfCRM.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW.oIHch2q0.js";import{_ as a}from"../app.CO8PvYbI.js";import"./chunk-5VM5RSS4.COy8Zt13.js";import"./chunk-XXDRQBXY.C8Yez1hl.js";import"./chunk-POPQ4Y6H.DPdQT8nk.js";import"./chunk-F27PBJKO.CWtVKMUR.js";import"./channel.hGTTk90g.js";import"./framework.DUdA5YZf.js";import"./theme.DJnBPmdL.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
