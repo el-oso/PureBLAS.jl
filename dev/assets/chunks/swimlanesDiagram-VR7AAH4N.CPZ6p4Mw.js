fdac643f934950a6c224c289a3a2d53c16ec256a
Documenter.jl <documenter@juliadocs.github.io>
2026-09-13 06:28:13 +0000

    build based on 5e99c90

---
+++ dev/assets/chunks/swimlanesDiagram-VR7AAH4N.CPZ6p4Mw.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.DAOd89g7.js";import{_ as a}from"../app.DdRztuyt.js";import"./chunk-5VM5RSS4.Bcd77sND.js";import"./chunk-XXDRQBXY.o3e9gKqh.js";import"./chunk-POPQ4Y6H.Bye1aNV9.js";import"./chunk-F27PBJKO.C7TIc-8P.js";import"./channel.BfTlAysK.js";import"./framework.DUdA5YZf.js";import"./theme.BKtAm6UP.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
